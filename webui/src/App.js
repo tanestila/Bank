31c09e02db5ee7cdc338bb0df626ae79fcbfa436
tanestila <millafko@gmail.com>
2018-09-23 16:17:58 +0700

    last

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,55 +1,63 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
-state = {
-  info : '',
-}
+  state = {
+    info: '',
+  }
 
-exportData = _ => {
-  fetch('http://127.0.0.1:4000/client/post', {
-  method: 'POST',
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    firstParam: 'yourValue',
-  }),
-});
-}
+  exportData = _ => {
+    fetch('http://127.0.0.1:4000/client/post', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstParam: 'yourValue',
+      }),
+    });
+  }
 
-importData = _ => {
-  
-  fetch(`http://127.0.0.1:4000/client/get?id=${this.refs.id.value}`)
-  .then(response => response.json())
-  .then(response=> this.setState({info: response.data.toString()}))
-  .catch(err => console.error(err));
-  console.log(this.state);
+  importData = _ => {
 
-}
+    fetch(`http://127.0.0.1:4000/client/get?id=${this.refs.id.value}`)
+      .then(response => response.json())
+      .then(response => this.setState({
+        info: response.data.toString()
+      }))
+      .catch(err => console.error(err));
+    console.log(this.state);
+
+  }
 
   render() {
-    const {info} = this.state;
+    const {
+      info
+    } = this.state;
     return (
 
-      <div className="App" >
+      <div className = "App" >
+        <form className = "form-group">
+        <pre > { info } </pre>
 
-      <plaintext >{info}</plaintext>
- <div className="form-group">
-   <label>ID</label>
-   <input  className="form-control"  ref="id" defaultValue="1" ></input>
- </div>
- <br />
-   <button className="btn  btn-primary" onClick={this.importData}> importData </button>
-   <br />
-   <form method="post" action="http://127.0.0.1:4000/client/post">
-    <textarea name="userInfo"></textarea><br />
-    <input className="btn  btn-primary"  type="submit" value="exportData" />
-</form>
-      </div>
+            <label > ID < /label>
+            <input className = "form-control" ref = "id" defaultValue = "1" ></input>
+
+          <br />
+          <input className = "btn  btn-primary"  onClick = { this.importData } value = "importData" />
+          <br />
+          </form>
+          <form method = "post" action = "http://127.0.0.1:4000/client/post" className = "form-group">
+            <textarea name = "userInfo" > < /textarea>
+            <br />
+            <input className = "btn  btn-primary" type = "submit"  value = "exportData" / >
+          </form>
+        </div>
     );
   }
 
